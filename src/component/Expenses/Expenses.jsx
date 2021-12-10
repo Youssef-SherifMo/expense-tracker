707a3e161c7d5a55272bd4231107c434f28f5d8c
youssef sherif <68399000+Youssef-SherifMo@users.noreply.github.com>
2021-12-10 17:02:42 +0200

    Adding Conditional Return Statements

--- src/component/Expenses/Expenses.jsx
+++ src/component/Expenses/Expenses.jsx
@@ -1,38 +1,28 @@
-import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
-import ExpensesFilter from "./ExpenseFilter";
-import Card from "../UI/Card";
-import "./Expenses.css";
+import React, { useState } from 'react';
+import ExpensesFilter from './ExpenseFilter';
+import ExpenseList from './ExpenseList';
+import Card from '../UI/Card';
+import './Expenses.css';
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("2020");
+	const [filteredYear, setFilteredYear] = useState('2020');
 
-  const filteredExpenses = props.items.filter((expense) => {
-    return expense.date.getFullYear().toString() === filteredYear;
-  });
-  const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
-  };
-  let expenseConent = <p>No Expense Found.</p>;
-  if (filteredExpenses.length > 0) {
-    expenseConent = filteredExpenses.map((item) => (
-      <ExpenseItem
-        key={item.id}
-        title={item.title}
-        amount={item.amount}
-        date={item.date}
-      />
-    ));
-  }
-  return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filteredYear}
-        onChangeFilter={filterChangeHandler}
-      />
-      {expenseConent}
-    </Card>
-  );
+	const filteredExpenses = props.items.filter((expense) => {
+		return expense.date.getFullYear().toString() === filteredYear;
+	});
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
+	return (
+		<Card className='expenses'>
+			<ExpensesFilter
+				selected={filteredYear}
+				onChangeFilter={filterChangeHandler}
+			/>
+			<ExpenseList items={filteredExpenses} />
+		</Card>
+	);
 }
 
 export default Expenses;
